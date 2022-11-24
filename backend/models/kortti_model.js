c0f25c2aa53d36a33f1ef0e9a9cca4924cfe0611
Janika Savela <janika_s@hotmail.com>
2022-11-24 20:42:14 +0200

    kortti taulun crud operaatiot toimii ja salasanan kryptaus

--- backend/models/kortti_model.js
+++ backend/models/kortti_model.js
@@ -1,5 +1,9 @@
 const db = require('../database');
 
+const bcrypt = require('bcryptjs');
+
+const saltRounds=10;
+
 const kortti = {
   getById: function(id, callback) {
     return db.query('select * from kortti where id_kortti=?', [id], callback);
@@ -8,21 +12,23 @@ const kortti = {
     return db.query('select * from kortti', callback);
   },
   add: function(add_data, callback) {
+    bcrypt.hash(add_data.pin_koodi, saltRounds, function(err, hashedPinKoodi){
     return db.query(
       'insert into kortti (id_kortti,pin_koodi,id_asiakas,credit) values(?,?,?,?)',
-      [add_data.id_kortti, add_data.pin_koodi, add_data.id_asiakas, add_data.credit],
-      callback
-    );
+      [add_data.id_kortti, hashedPinKoodi, add_data.id_asiakas, add_data.credit],
+      callback);
+    });
   },
   delete: function(id, callback) {
     return db.query('delete from kortti where id_kortti=?', [id], callback);
   },
   update: function(id, update_data, callback) {
+    bcrypt.hash(update_data.pin_koodi, saltRounds, function(err, hashedPinKoodi){
     return db.query(
       'update kortti set pin_koodi=?,id_asiakas=?, credit=? where id_kortti=?',
-      [update_data.pin_koodi, update_data.id_asiakas, update_data.credit, id],
-      callback
-    );
+      [hashedPinKoodi, update_data.id_asiakas, update_data.credit, id],
+      callback);
+    });
   }
 };
 module.exports = kortti;
